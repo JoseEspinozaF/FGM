5ee6d7fd920d16b5baeaa55a382c6fa9819284bf
Jose Antonio Espinoza Franco <josefr7h@gmail.com>
2022-04-01 20:36:56 -0400

    Change function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,10 @@
-let img, canvas, ctx, posX = 0,
-    posy = 0
 window.onload = function () {
+    let img, canvas, ctx, posX = 0,
+        posy = 0
     canvas = document.querySelector(".content-canvas")
     ctx = canvas.getContext('2d')
     img = new Image();
     img.src = '../img/post.jpg'
-    animar()
-
-
-}
-
-function animar() {
     ctx.drawImage(img, posX, posy, 40, 40);
     ctx.clearRect(0, 0, 700, 800);
     posX += 20
@@ -19,4 +13,6 @@ function animar() {
     }
 
     setTimeout(animar, 100);
+
+
 }
